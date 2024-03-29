a8817a05c9a54dd9f486bba99a02af4d2b38e3f2
Yusufhoja <usufhozah@gmail.com>
2024-03-29 14:11:04 +0500

    js 13-dars

---
+++ script.js
@@ -0,0 +1,21 @@
+// let a="name:"
+// let b="idi:"
+// let c="password:"
+// let d=prompt("ismingizni kiriting");
+// let e=+prompt("idiingizni kiriting");
+// let f=+prompt("passwordingizni kiriting");
+// console.log(`${a}${d}`);
+// console.log(`${b} ${e}`);
+// console.log(`${c} ${f}`);
+
+
+
+let name = prompt('Ismingizni kiriting:');
+let password = prompt('Parolingizni kiriting:');
+let idi= prompt("idi ingizni kiriting")
+let user = {
+  name: name,
+  password: password,
+  idi: idi
+};
+console.log(user);
